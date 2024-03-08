f4ae588e7a988fbb3ac2dc9a630375f4d3d96a9c
martinadamsUL <martin.adams@ul.com>
2024-03-08 10:13:57 +0000

    fix: :pencil2: lsagent file exist check was not in correct case

--- lib/facter/lsagent_path.rb
+++ lib/facter/lsagent_path.rb
@@ -2,6 +2,6 @@ Facter.add(:lsagent_path) do
   confine { Facter.value(:kernel) != 'windows' }
   confine { Facter.value(:operatingsystem) != 'nexus' }
   setcode do
-    File.exists?('/opt/LansweeperAgent/LsAgent')
+    File.exists?('/opt/LansweeperAgent/LSAgent')
   end
 end
